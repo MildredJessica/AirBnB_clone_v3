7f9ef5042b67993142734f0efef00ff351506525
MildredJessica <nwonyejessica@gmail.com>
2023-08-01 05:09:55 +0100

    404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """API module"""
 
-from flask import Flask, Blueprint
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -16,8 +16,16 @@ def teardown_app(exception):
     """Handles what happens when the application context is popped """
     storage.close()
 
+    @app.errorhandler(404)
+    def error_handler(code):
+        """Handles a 404 error"""
+        error = {
+            "error": "Not found"
+        }
+        return jsonify(error, 404)
+
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', strict_slash=False)
@@ -12,3 +13,19 @@ def status():
         "status": "OK"
     }
     return jsonify(status)
+
+
+@app_views.route('/stat', strict_slash=False)
+def count():
+    """Retrieves the number of each objects by type"""
+    models_avail = {
+        "User": "users",
+        "Amenity": "amenities", "city": "cities",
+        "Place": "places", "Review": "reviews",
+        "State": "states",
+    }
+
+    count = {}
+    for cls in models_avail.keys():
+        count[models_avail[cls]] = storage.count(cls)
+    return jsonify(count)
