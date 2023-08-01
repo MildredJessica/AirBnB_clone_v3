3f856c8e917dd26dd749e9a633d6c92b25b88da6
MildredJessica <nwonyejessica@gmail.com>
2023-08-01 23:14:01 +0100

    Added api folder

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+"""Blueprint Initialization"""
+
+from flask import Blueprint
+from api.v1.views import *
+from api.v1.views import states
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
